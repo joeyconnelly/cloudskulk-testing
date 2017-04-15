5637f3ad8960ac74a83b107c16324fac1352acb9
Joseph Connelly <joeyconnelly@u.boisestate.edu>
2017-04-15 17:35:08 -0600

    baremetal updated quick script + add base datagit add *!

--- io-tests/quick.sh
+++ io-tests/quick.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#file=joey-4_15-bareMetal.data
+#file=joey-4_15-level1-12G-writeback.data
+file=joey-4_15-level1-12G-writthrough.data
 
-/usr/local/bin/filebench -f cloud_fileserver.f 2> /dev/null 1> joey-4_15-level1-12G-writeback.data
+/usr/local/bin/filebench -f cloud_fileserver.f 2> /dev/null 1> $file
 
+cat 
