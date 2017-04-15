6ad0e44d6304f909fb153f79fbdddc0efee11f4b
Joseph Connelly <joeyconnelly@u.boisestate.edu>
2017-04-15 17:44:32 -0600

    level1 writeback seems ~faster than writethrough

--- io-tests/quick.sh
+++ io-tests/quick.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #file=joey-4_15-bareMetal.data
+### writeback seems to be a little faster than writethrough...
 #file=joey-4_15-level1-12G-writeback.data
-file=joey-4_15-level1-12G-writthrough.data
+#file=joey-4_15-level1-12G-writethrough.data
+
+file=joey-4_15-level1-6G-writeback.data
+#file=joey-4_15-level1-15G-writeback.data
 
 /usr/local/bin/filebench -f cloud_fileserver.f 2> /dev/null 1> $file
 
-cat 
+cat $file
