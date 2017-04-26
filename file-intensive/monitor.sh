faa317e85c34f9f28764177ff1fbdeb4ae7cac13
Testing Cloud Level1 <level1@localhost.localdomain>
2017-04-25 23:07:52 -0600

    level1 page-fault data taken

--- file-intensive/monitor.sh
+++ file-intensive/monitor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 file=timeGuest.txt
-prog=page-faults
+prog=./page-faults
 #prog=/home/jconnell/research/level1-testFilebench
 
-rm -f $subfile $file
+rm -f $file
 echo -e "start monitoring page faults..."
 /usr/bin/time -v $prog 2> $file 
 echo -e "stop monitoring page faults..."
