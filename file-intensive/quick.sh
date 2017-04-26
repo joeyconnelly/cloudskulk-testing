23c39f86c0a64a9ebc8dead6573e91c3c42f9bb5
Test Level1 30G <level1-30g@localhost.localdomain>
2017-04-26 12:58:07 -0600

    finding what guest img size does to filebench timing

---
+++ file-intensive/quick.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+file=guest30G-quick.data
+echo -e "starting script... \n"
+/usr/local/bin/filebench -f def_fileserver.f 1> $file
+/usr/local/bin/filebench -f def_varmail.f 1>> $file
+/usr/local/bin/filebench -f def_webserver.f 1>> $file
+echo -e "\nending script..."
