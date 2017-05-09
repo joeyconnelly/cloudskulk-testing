0d28c80786a44e499151534272152b6fd3937ac5
Level1 60G QCOW2 <level1-60g-qcow@localhost.localdomain>
2017-05-09 16:56:45 -0600

    quick testing files added to find out cache=none, io=native/aio changes timing

--- file-intensive/quick.sh
+++ file-intensive/quick.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-run=0
-tag=14G-60Graw
-
+tag=unsafe
 echo -e "starting script... \n"
-if [ $run == 0 ];then
-	file=dataQuick-$tag-lit.data
-	/usr/local/bin/filebench -f literature_fileserver.f 1> $file
-	/usr/local/bin/filebench -f literature_varmail.f 1>> $file
-	/usr/local/bin/filebench -f literature_webserver.f 1>> $file
-elif [ $run = 1 ];then
-	file=dataQuick-$tag-def.data
-	/usr/local/bin/filebench -f _fileserver.f 1> $file
-	/usr/local/bin/filebench -f _varmail.f 1>> $file
-	/usr/local/bin/filebench -f _webserver.f 1>> $file
-fi
+file=dataQuick-$tag.data
+/usr/local/bin/filebench -f working3-1G.f 1> $file
+/usr/local/bin/filebench -f working6-25G.f 1>> $file
 echo -e "\nending script..."
 
---
+++ file-intensive/working6-25G.f
@@ -0,0 +1,33 @@
+
+set $myset=bigfileset
+set $mylog=logfiles
+set $dir=/tmp
+set $filesize=64k
+set $nfiles=50000
+set $meandirwidth=20
+set $nthreads=50
+set $nprocesses=1
+set $iosize=1m
+set $meanappendsize=16k
+
+define fileset name=$myset,path=$dir,size=$filesize,entries=$nfiles,dirwidth=$meandirwidth,prealloc=80
+
+define process name=filereader,instances=$nprocesses
+{
+  thread name=filereaderthread,memsize=10m,instances=$nthreads
+  {
+    flowop createfile name=createfile1,filesetname=$myset,fd=1
+    flowop writewholefile name=wrtfile1,srcfd=1,fd=1,iosize=$iosize
+    flowop closefile name=closefile1,fd=1
+    flowop openfile name=openfile2,filesetname=$myset,fd=1
+    flowop appendfilerand name=appendfilerand1,iosize=$meanappendsize,fd=1
+    flowop readwholefile name=readfile1,fd=1,iosize=$iosize
+    flowop closefile name=closefile3,fd=1
+    flowop deletefile name=deletefile1,filesetname=$myset
+    flowop statfile name=statfile1,filesetname=$myset
+  }
+}
+
+echo  "File-server Version 3.0 personality successfully loaded"
+
+run 60
