1d25b5b38e2747c52fb94d2394696ad0fc08f746
Level1 60G QCOW2 <level1-60g-qcow@localhost.localdomain>
2017-05-15 13:20:28 -0600

    before newSmhoo with homeDir filebench path

--- file-intensive/shmoo.sh
+++ file-intensive/shmoo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 numRuns=1
-tag=_2gvdram_60gqcow_virtio_writethrough_threads_homeDir
+tag=_2to4g_60gqcow_virtio_writeback_threads_homeDir
 min=1
 max=128
 # nfiles=50000, filesize_max=128k, totalSize=>6.4G
@@ -14,12 +14,12 @@ inputText="bigfileset populated:"
 outputText="IO Summary:"
 runProg="/usr/local/bin/filebench -f"
 declare -a testFiles=(
-	"adjustNOAPPEND_fileserver.f"
 	"adjustAPPEND_16_fileserver.f"
-	"adjustAPPEND_64_fileserver.f"
-	"adjustNOAPPEND_webserver.f"
+	"adjustNOAPPEND_fileserver.f"
 	"adjustAPPEND_16_webserver.f"
-	"adjustAPPEND_64_webserver.f"
+	"adjustNOAPPEND_webserver.f"
+	"adjustAPPEND_16_varmail.f"
+	"adjustNOAPPEND_varmail.f"
 )
 declare -a benchFiles=(
 	"$HOME/testing/bigfileset"
@@ -62,7 +62,7 @@ do
 		newText+="k"
 		sed --in-place "/$sizeText/c$newText" $myTest
 		newText="$pathText$HOME"
-		newText+="testing"
+		newText+="/testing"
 		sed --in-place "/$pathText/c$newText" $myTest
 
 		#
