41cf6b3717ca659e3ba669dafc1934fe52f593b0
Level1 RAW 60G <level1-60graw@localhost.localdomain>
2017-05-15 18:45:25 -0600

    MACRO level1/raw e1000-net

--- file-intensive/test-macros.sh
+++ file-intensive/test-macros.sh
@@ -2,7 +2,7 @@
 insideVM=1
 numRuns=1
 #tag=_host_cfq_homeDir
-tag=_level1_cfq_raw2_60G_virtio-net_2to4G_threads_writeback
+tag=_level1_cfq_raw_60G_e1000-net_2to4G_threads_writeback
 fileName=data_
 tempCopy=temp
 errorLog=errFile.log
