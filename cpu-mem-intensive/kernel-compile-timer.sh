62941e06db85b8c1b27fa1fe8328808e1a9951e1
Level2 60G Raw <level2-guest@localhost.localdomain>
2017-05-19 15:31:31 -0600

    kernel avg level2

--- cpu-mem-intensive/kernel-compile-timer.sh
+++ cpu-mem-intensive/kernel-compile-timer.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
 
-#tag=host
-#tag=level1
-tag=level2
+numRuns=4
+#tag=host-avg
+#tag=level1-avg
+tag=level2-avg
 link="https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.11.1.tar.xz"
 file="$tag-compile-timing.data"
 version="linux-4.11.1"
 download=".tar.xz"
 
-
+#
+# Download kernel version.
+#
 repoDir=$(pwd)
-cd $HOME
 echo -e $tag > $repoDir/$file
+cd $HOME
 wget $link
-tar -xvf $version$download
-cd $version
-#myKern=$(uname --kernel-release)
-#cp /boot/config-$myKern .config
-make defconfig
 
+#
+# Compile the kernel + save build times.
+#
+for (( i=0; i<$numRuns; i++ ))
+do
+	tar -xvf $version$download
+	cd $version
+	make defconfig
 
-startTime=$(date +%s.%N)
-make -j4 
-endTime=$(date +%s.%N)
+	startTime=$(date +%s.%N)
+	make -j4 
+	endTime=$(date +%s.%N)
 
-
-elapsedTime=$(python -c "print(${endTime} - ${startTime})")
-echo -e "Start_Time(seconds),$startTime"
-echo -e "Start_Time(seconds),$startTime" >> $repoDir/$file
-echo -e "End_Time(seconds),$endTime"
-echo -e "End_Time(seconds),$endTime" >> $repoDir/$file
-echo -e "Elapsed_Time(seconds),$elapsedTime"
-echo -e "Elapsed_Time(seconds),$elapsedTime" >> $repoDir/$file
+	elapsedTime=$(python -c "print(${endTime} - ${startTime})")
+	echo -e "Run[$i],Start_Time(seconds),$startTime"
+	echo -e "Run[$i],Start_Time(seconds),$startTime" >> $repoDir/$file
+	echo -e "Run[$i],End_Time(seconds),$endTime"
+	echo -e "Run[$i],End_Time(seconds),$endTime" >> $repoDir/$file
+	echo -e "Run[$i],Elapsed_Time(seconds),$elapsedTime"
+	echo -e "Run[$i],Elapsed_Time(seconds),$elapsedTime" >> $repoDir/$file
+	
+	cd $HOME
+	rm -rf $version
+done
 
