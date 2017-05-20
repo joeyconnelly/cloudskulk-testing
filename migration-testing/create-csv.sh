18968bb161e782a86c76e4ac36e0181566150d4c
Joseph Connelly <joeyconnelly@u.boisestate.edu>
2017-05-20 16:22:01 -0600

    host script + idle + kernel data

---
+++ migration-testing/create-csv.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+input="bareMetal-kernel.raw"
+output=${input%.*}
+output+=".csv"
+temp=trash
+
+rm -f $output $temp
+echo -e "bareMetal-to-bareMetal Idle Workload" > $output
+grep -E 'total time:' $input > $temp
+echo -e "End-to-End_Timing:" >> $output
+awk -F ' ' '{print $3}' $temp 1>> $output
+
+grep -E 'downtime:' $input > $temp
+echo -e "Downtime:" >> $output
+awk -F ' ' '{print $2}' $temp 1>> $output
+
+rm -f $temp
+cat $output
+
