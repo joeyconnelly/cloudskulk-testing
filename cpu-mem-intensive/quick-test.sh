ceaf13903b9b71ec300bfe4f4bd46692f0fc19de
Joseph Connelly <joeyconnelly@u.boisestate.edu>
2017-05-26 18:31:45 -0600

    host config

--- cpu-mem-intensive/quick-test.sh
+++ cpu-mem-intensive/quick-test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 numRuns=3
-testTag="L1 -> vDRAM=2G, cpus8, numa=def, make -j8"
-tag=experimentingG-quick-level1
+testTag="host nested config"
+tag=sdafasfexperimentingA-quick-host
 link="https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.11.1.tar.xz"
 file="$tag-compile-timing.data"
 version="linux-4.11.1"
@@ -11,14 +11,14 @@ download=".tar.xz"
 repoDir=$(pwd)
 echo -e $testTag > $repoDir/$file
 echo -e $tag >> $repoDir/$file
-for (( i=0; i<$numRuns; i++ ))
-do
+#for (( i=0; i<$numRuns; i++ ))
+#do
 	cd $HOME
-	rm -rf $version
-	tar -xvf $version$download
+#	rm -rf $version
+#	tar -xvf $version$download
 	cd $version
-	make defconfig &>/dev/null
-
+#	make defconfig &>/dev/null
+	make clean
 	echo -e "Run[$i]..."
 	startTime=$(date +%s.%N)
 	make -j8 &>/dev/null
@@ -31,5 +31,5 @@ do
 	echo -e "Run[$i],End_Time(seconds),$endTime" >> $repoDir/$file
 	echo -e "Run[$i],Elapsed_Time(seconds),$elapsedTime"
 	echo -e "Run[$i],Elapsed_Time(seconds),$elapsedTime" >> $repoDir/$file
-done
+#done
 
