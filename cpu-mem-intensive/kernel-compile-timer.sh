54b43fe1edf4099bbe6c9e23e366afd40d92a567
Joseph Connelly <joeyconnelly@u.boisestate.edu>
2017-05-27 17:44:43 -0600

    host kernel NEW decompression data

--- cpu-mem-intensive/kernel-compile-timer.sh
+++ cpu-mem-intensive/kernel-compile-timer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-numRuns=1
-tag=level1-not-working
+numRuns=5
+tag=level1-all
 link="https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.0.tar.xz"
 file="$tag-build.data"
 version="linux-4.0"
@@ -22,16 +22,23 @@ fi
 
 for (( i=0; i<$numRuns; i++ ))
 do
+	if [ $i = 1 ];then
+		echo "Do you want to continue (y|n): "
+		read input_variable
+		if [ "$input_variable" == "n" ];then
+			exit -1
+		fi
+	fi
 	rm -rf $version
 
 	startDecompress=$(date +%s.%N)
 	tar -xvf $version$download
 	endDecompress=$(date +%s.%N)
 	totalDecompress=$(python -c "print(${endDecompress} - ${startDecompress})")
-	echo -e "\n\n\nRun[$i],$startDecompress,$endDecompress,$totalDecompress\n\n"
+	echo -e "\n\n\nDecompressRun[$i],$startDecompress,$endDecompress,$totalDecompress\n\n\n"
 	
 	cd $version
-	if [ "$firstRun" = true ];then
+	if [ "$firstRun" = true -a $i = 0 ];then
 		make menuconfig
 		cp .config $repoDir
 	else
@@ -40,14 +47,15 @@ do
 	if [ "$virtEnv" = true ];then
 		cd include/linux/
 		ln -s compiler-gcc5.h compiler-gcc6.h
-		cd -
+		cd ../../
 	fi
 	
+	make clean
 	startCompile=$(date +%s.%N)
 	make -j8
 	endCompile=$(date +%s.%N)
 	totalCompile=$(python -c "print(${endCompile} - ${startCompile})")
-	echo -e "\n\n\nRun[$i],$startCompile,$endCompile,$totalCompile\n\n"
+	echo -e "\n\n\nCompileRun[$i],$startCompile,$endCompile,$totalCompile\n\n"
 	
 	echo -e "Run[$i],$startDecompress,$endDecompress,$totalDecompress,$startCompile,$endCompile,$totalCompile" >> $repoDir/$file
 	cd $HOME
