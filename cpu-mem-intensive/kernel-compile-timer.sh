450109beb75b9eef4a7be4ef5c1253e1627ffd99
Joseph Connelly <joeyconnelly@u.boisestate.edu>
2017-05-27 13:57:38 -0600

    NEW kernel compile data - host

--- cpu-mem-intensive/kernel-compile-timer.sh
+++ cpu-mem-intensive/kernel-compile-timer.sh
@@ -1,33 +1,51 @@
 #!/bin/bash
 
-numRuns=4
-#tag=host-avg
-tag=level1-avg
-#tag=level2-avg
-link="https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.11.1.tar.xz"
-file="$tag-compile-timing.data"
-version="linux-4.11.1"
+numRuns=3
+testTag="kernel compile data in level1 120G"
+tag=level1-gold
+link="https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.0.tar.xz"
+file="$tag-build.data"
+version="linux-4.0"
 download=".tar.xz"
+firstRun=false
 
 #
-# Download kernel version.
+# Initial files and script directory location.
 #
 repoDir=$(pwd)
-echo -e $tag > $repoDir/$file
+echo -e $headerTag > $repoDir/$file
+echo -e $tag >> $repoDir/$file
+
+#
+# Download kernel version if not found.
+#
 cd $HOME
-wget $link
+if [ ! -f $version$download ];then
+	wget $link
+fi
+rm -rf $version
+tar -xvf $version$download
+
+#
+# Use the same host config file for all.
+#
+cd $version
+if [ "$firstRun" = true ];then
+	make menuconfig
+	cp .config $repoDir
+else
+	cp $repoDir/.config .
+fi
 
 #
 # Compile the kernel + save build times.
 #
 for (( i=0; i<$numRuns; i++ ))
 do
-	tar -xvf $version$download
-	cd $version
-	make defconfig
+	make clean
 
 	startTime=$(date +%s.%N)
-	make -j4 
+	make -j8
 	endTime=$(date +%s.%N)
 
 	elapsedTime=$(python -c "print(${endTime} - ${startTime})")
@@ -37,8 +55,5 @@ do
 	echo -e "Run[$i],End_Time(seconds),$endTime" >> $repoDir/$file
 	echo -e "Run[$i],Elapsed_Time(seconds),$elapsedTime"
 	echo -e "Run[$i],Elapsed_Time(seconds),$elapsedTime" >> $repoDir/$file
-	
-	cd $HOME
-	rm -rf $version
 done
 
