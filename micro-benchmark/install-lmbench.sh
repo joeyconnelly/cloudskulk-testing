c8db07e6bb3e1784897fd51e3f2b1bc7c6ffbb94
Joseph Connelly <joeyconnelly@u.boisestate.edu>
2017-05-31 22:23:50 -0600

    updated lmbench script

--- micro-benchmark/install-lmbench.sh
+++ micro-benchmark/install-lmbench.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+tag=level1
+header="LMBench FINISHED: $tag"
+email=joeyconnelly@u.boisestate.edu
+file=/tmp/email.txt
 version="lmbench-3.0-a9"
 download="lmbench-3.0-a9.tgz"
 dir=$HOME
@@ -8,16 +12,22 @@ dir=$HOME
 repoDir=$(pwd)
 cd $dir
 if [ ! -f "$download" ];then
-	echo -e "Download not found..."
+	echo -e "Download not found... downloading now."
 	cp $repoDir/$download $dir
 fi
 if [ ! -d "$version" ];then
-	echo -e "Directory not found..."
+	echo -e "Directory not found... creating now."
 	tar -xvzf $repoDir/$download -C $dir
 fi
 cd $version/src
 
 make results
 
+# To run benchmark with same configs multiple times:
 #make return
+
+cp ../results/x86_64-linux-gnu/* $repoDir
+
+echo -e "$header" > $file
+sendmail $email < $file
 
